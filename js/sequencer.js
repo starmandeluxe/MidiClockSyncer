75c75362a3fef090e188e87799a9548df2d31471
Alex K <starmandeluxe2001@gmail.com>
2015-10-25 12:05:18 +0900

    removed note vars, added cc midi message output

--- js/sequencer.js
+++ js/sequencer.js
@@ -14,56 +14,14 @@ var nextClockTime = 0.0;     // when the next note is due.
 var startTime = 0;
 var playPressed = false;
 var isPlaying = false;
-
 var beatCounter = 0;
+var beatDivider = 24;
 
-//Midi note mappings
-var notes = {
-'C0':'12','C#0':'13','D0':'14','D#0':'15',
-'E0':'16','F0':'17','F#0':'18','G0':'19',
-'G#0':'20','A0':'21','A#0':'22','B0':'23',
-
-'C1':'24','C#1':'25','D1':'26','D#1':'27',
-'E1':'28','F1':'29','F#1':'30','G1':'31',
-'G#1':'32','A1':'33','A#1':'34','B1':'35',
-
-'C2':'36','C#2':'37','D2':'38','D#2':'39',
-'E2':'40','F2':'41','F#2':'42','G2':'43',
-'G#2':'44','A2':'45','A#2':'46','B2':'47',
-
-'C3':'48','C#3':'49','D3':'50','D#3':'51',
-'E3':'52','F3':'53','F#3':'54','G3':'55',
-'G#3':'56','A3':'57','A#3':'58','B3':'59',
-
-'C4':'60','C#4':'61','D4':'62','D#4':'63',
-'E4':'64','F4':'65','F#4': '66','G4': '67',
-'G#4':'68','A4':'69','A#4':'70','B4':'71',
-
-'C5':'72','C#5':'73','D5':'74','D#5':'75',
-'E5':'76','F5':'77','F#5':'78','G5':'79',
-'G#5':'80','A5':'81','A#5':'82','B5':'83',
-
-'C6':'84','C#6':'85','D6':'86','D#6':'87',
-'E6':'88','F6':'89','F#6':'90','G6':'91',
-'G#6':'92','A6':'93','A#6':'94','B6':'95',
-
-'C7':'96','C#7':'97','D7':'98','D#7':'99',
-'E7':'100','F7':'101','F#7':'102','G7':'103',
-'G#7':'104','A7':'105','A#7':'106','B7':'107',
-
-'C8':'108','C#8':'109','D8':'110','D#8':'111',
-'E8':'112','F8':'113','F#8':'114','G8':'115',
-'G#8':'116','A8':'117','A#8':'118','B8':'119',
-
-'C9':'120','C#9':'121','D9':'122','D#9':'123',
-'E9':'124','F9':'125','F#9':'126','G9':'127'
-};
-
-var note1 = notes['C4'];
+var cc = [0x08];
 
 //Actions to perform on load
 window.addEventListener('load', function() {
-    
+
     //Prevent swipe down to refresh in Android Chrome 
     var lastTouchY = 0;
     var touchstartHandler = function(e) {
@@ -206,7 +164,7 @@ function stop() {
     window.clearTimeout(timerID);
 }
 
-
+//Schedules when the next clock should fire
 function scheduleClock() {
     var currentTime = audioContext.currentTime;
     currentTime -= startTime;
@@ -219,6 +177,9 @@ function scheduleClock() {
                 playPressed = false;
                 midiDevice.send([0xFA]);
                 midiDevice.send([0xF8]);
+                //Send C0 for Doepfer A-190-2 Midi Interface Learn
+                midiDevice.send([0x90, 12, 0x40]);
+                midiDevice.send([0x80, 12, 0x40]);
             }, currentTime + nextClockTime);
          }
         advanceClock();
@@ -226,25 +187,17 @@ function scheduleClock() {
     timerID = window.setTimeout("scheduleClock()", 0);
 }
 
+//move the clock forward by tempo intervals (24ppq)
 function advanceClock() {
     //send midi clock signal
     midiDevice.send([0xF8]);
-    //advance beat
+    //send cc for Doepfer A-190-2 Midi Interface
+    if (beatCounter % beatDivider == 0) {     
+        midiDevice.send([0xB0, cc, 0x7F]);
+        midiDevice.send([0xB0, cc, 0x00]); 
+    }
     beatCounter++;
-    if (beatCounter == 192) {
-        beatCounter = 0;
-    }
-    //eighth notes
-    if (beatCounter % 12 == 0) {
-        if (note1) {
-            //turn off note
-            midiDevice.send([0x80, note1, 0x40]);
-            //console.log('Stopped note ' + note1);
-        }
-        //turn on note
-        midiDevice.send([0x90, note1, 0x7f]);
-        //console.log('Sent note ' + note1);
-    }
+
     //the next clock will be at the next tempo marker
     nextClockTime += tempo;
 }
